18bb568f32deeaf809227fedc551b297b3dd235e
marwanmar123 <youcode098@gmail.com>
2021-03-12 15:47:43 +0100

    add current reservation

---
+++ Resarvation/Controllers/ReservationController.cs
@@ -0,0 +1,145 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Resarvation.Data;
+using Resarvation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Resarvation.Controllers
+{
+    public class ReservationController : Controller
+    {
+        ApplicationDbContext _db;
+
+        public ReservationController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        // GET: ReservationController
+        public ActionResult Index()
+        {
+            var Result = (from r in _db.Reservations
+                          join s in _db.Apprenants
+                          on r.Apprenant.Id equals s.Id
+                          join rt in _db.TypeReservations
+                          on r.TypeReservation.Id equals rt.Id
+
+                          select new ReservApprenantViewModel
+                          {
+                              Id = s.Id,
+                              UserName = s.UserName,
+                              Email = s.Email,
+                              Date = r.Date,
+                              Cause = r.Cause,
+                              Status = r.Status,
+                              TypeReservation = rt.Name,
+                          }).ToList();
+
+            return View("Index", Result);
+        }
+
+
+        public ActionResult Reserv()
+        {
+
+            var user = User.Identity.Name;
+            var Result = (from r in _db.Reservations
+                          join s in _db.Apprenants
+                          on r.Apprenant.Id equals s.Id
+                          join rt in _db.TypeReservations
+                          on r.TypeReservation.Id equals rt.Id
+                          where s.UserName == user
+                          select new ReservApprenantViewModel
+                          {
+                              Id = s.Id,
+                              UserName = s.UserName,
+                              Email = s.Email,
+                              Date = r.Date,
+                              Cause = r.Cause,
+                              Status = r.Status,
+                              TypeReservation = rt.Name,
+                          }).ToList();
+
+            return View("Index", Result);
+        }
+
+
+        //public IActionResult Search(string term)
+        //{
+        //    var result = _db.Reservations.Where(b => b.Date == ).ToList();
+        //    return View("Index", result);
+        //}
+
+        // GET: ReservationController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ReservationController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ReservationController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ReservationController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ReservationController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ReservationController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ReservationController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- Resarvation/Models/ReservApprenantViewModel.cs
+++ Resarvation/Models/ReservApprenantViewModel.cs
@@ -5,14 +5,16 @@ using System.Threading.Tasks;
 
 namespace Resarvation.Models
 {
-    public class Reservation
+    public class ReservApprenantViewModel
     {
         public string Id { get; set; }
+        public string UserName { get; set; }
+        //public string LastName { get; set; }
+        public string Email { get; set; }
         public DateTime Date { get; set; }
         public string Status { get; set; }
         public string Cause { get; set; }
-
-        public Apprenant Apprenant { get; set; }
-        public TypeReservation TypeReservation { get; set; }
+        //public string SearchString { get; set; }
+        public string TypeReservation { get; set; }
     }
 }
